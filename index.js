569f4bbdf245b8d980e7b2863526eae0eaf181ae
Shippey <BENJAMIN_W_SHIPPEY@homedepot.com>
2018-08-25 11:57:37 -0400

    Added CSS, Dynamic HTML for dropdown form options on index page, introduced promise.all handling for MongoDB queries

--- index.js
+++ index.js
@@ -7,28 +7,74 @@ const app = express();
 /*
 Referenced documentation here for MongoDB Node.JS Client (API Docs aren't very descriptive)
 http://mongodb.github.io/node-mongodb-native/3.1/quick-start/quick-start/
+
+API Docs: http://mongodb.github.io/node-mongodb-native/3.1/api/
 */
-
-//set the templating engine to be Pug
-app.set('views','views');
-app.set('view engine', 'pug')
-
-
-app.get("*", (req,res) => res.render('index.pug'))
 
 //set MongoDB Connection URI
 var mongoURI = `mongodb://${process.env.user}:${process.env.password}@ds131621.mlab.com:31621/nationalparks`;
 
-app.post("/results", upload.array() ,(req,res) => {
+//set the templating engine to be Pug
+app.set('views','views');
+app.set('view engine', 'pug');
 
-  //connect to MongoDB using Mongo URI
-  MongoClient.connect(mongoURI,(err,client) => {
-    database = client.db('nationalparks');
-    collection = database.collection('species');
-    collection.find({}).map((collection) => {return {"Scientific Name":1,"Common Names":2, "Category":3, "Abundance":4}}).limit(100).toArray((error,result) => {
-      console.log(result);
-      res.render('results', {query:result});
+var resourceMiddleware = express.static('resources'); //construct function that serves static files from internal 'resources' folder
+app.use("/resources",resourceMiddleware); //'mount' the  function to trigger at the external '/resources' path
+
+app.get("/test", (req,res) => res.render('index-test.pug'))
+
+app.get("/index", (req,res) => {
+  MongoClient.connect(mongoURI, (err,client) => {
+    var database = client.db("nationalparks");
+    var collection = database.collection("species");
+
+    var parkNames = collection.distinct("Park Name");
+    var categories = collection.distinct("Category");
+
+    Promise.all([parkNames , categories]).then(results => {
+      res.render('index.pug', {parks:results[0], categories:results[1]});
     });
+    client.close();
+  })
+})
+
+app.post("/results", upload.array() ,(req,res) => {
+  //connect to MongoDB using Mongo URI
+
+  console.log(req.body.category);
+
+  MongoClient.connect(mongoURI,(err,client) => {
+    var database = client.db('nationalparks');
+    var collection = database.collection('species');
+
+
+    var queryCriteria = {
+      "Park Name":req.body.park,
+      "Category":req.body.category
+    };
+
+    //get query result count
+    var count = collection.find(queryCriteria).count();
+
+    //get query result
+    var model = collection.find(queryCriteria)
+        .skip(parseInt(req.body.index))
+        .limit(10)
+        .toArray()
+
+    //work with query results
+    Promise.all([model, count]).then((results) => {
+      var lowerBound = parseInt(req.body.index)
+      var recordCount = results[1];
+      var upperBound = (lowerBound + 10 < recordCount) ? (lowerBound +  10) : recordCount;
+      res.render('results', {
+        query:results[0],
+        lowerBound:lowerBound,
+        count:results[1],
+        upperBound:upperBound,
+        park:req.body.park,
+        category:req.body.category});
+    })
     client.close();
   });
 })
